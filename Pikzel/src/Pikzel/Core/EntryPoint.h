bd03ffd3081573c3d9a006c738a3efb1b1676dd1
0x <jrfwaite@gmail.com>
2020-11-15 14:25:59 +1300

    Make winmain() (currently unused) consistent with main()
    - just tidying it up for later

--- Pikzel/src/Pikzel/Core/EntryPoint.h
+++ Pikzel/src/Pikzel/Core/EntryPoint.h
@@ -6,34 +6,24 @@
 // Must be defined in client
 std::unique_ptr<Pikzel::Application> CreateApplication(int argc, const char* argv[]);
 
-// Later, we might consider making it a windows app instead of console...
-// but until such time as we have a good place to send stdout to, lets
-// just keep it as console
-//
-// Note: making it a windows app also requires WIN32 added to the add_executable command
-//       in CMakeLists.txt
-#ifdef PKZL_PLATFORM_WINDOWS
-#if 0
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-   Pikzel::Log::Init();
-   try {
-      CreateApplication(__argc, (const char**)__argv)->Run();
-   } catch (std::exception err) {
-      CORE_LOG_FATAL(err.what());
-      return EXIT_FAILURE;
-   }
-   return EXIT_SUCCESS;
-}
-#endif
-#endif
+// Note: Windowed app also also requires WIN32 added to the add_executable command in CMakeLists.txt
+#if defined(PKZL_PLATFORM_WINDOWS) && defined(PKZL_BUILD_WINDOWED_APP)
+#define ARGC __argc
+#define ARGV (const char**)__argv
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#else
+#define ARGC argc
+#define ARGV argv
 
 int main(int argc, const char* argv[]) {
+#endif
    Pikzel::Log::Init();
    try {
       Pikzel::EventDispatcher::Init();
-      CreateApplication(argc, argv)->Run();
-      
+      CreateApplication(ARGC, ARGV)->Run();
+
       // The EventDispatcher needs to be destructed before unloading the RenderCore api shared library
       // (because the render core can (and does) register event classes with the dispatcher.
       // If you unload the shared library, then these classes are no longer defined, and destructing
